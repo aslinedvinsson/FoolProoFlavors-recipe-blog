f81b303e7c63df76df0b28f45c046bb268ebe51e
Ann Edvinsson <aslin.edvinsson@gmail.com>
2024-01-26 07:49:59 +0000

    Add RecipePost model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,51 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+STATUS = ((0, "Draft"), (1, "Published"))
+
+MEAL_TYPES = (("chicken", "Chicken"),("fish", "Fish"), ("meat", "Meat"),
+("vegitarian", "Vegitarian"), ("vegan", "Vegan"))
+
+EFFORT = (("bad_day_comfort_food", "Bad day comfort food"),
+("trying_a_healthy_day", "Trying a healthy day"), ("im_in_a_hurry", "I'm in a hurry"),
+("i_have_time_but_no_brains", "I have time but no brains"))
+
+class RecipePost(models.Model):
+    """
+    A model to create and manage a recipe post related to :model:`auth.User`
+    """
+
+    user = models.ForeignKey(
+        User, related_name="blog_posts", on_delete=models.CASCADE
+    )
+    title = models.CharField(max_length=200, null=False, blank=False)
+    slug = models.SlugField(max_length=200, unique=True)
+    #featured_image = CloudinaryField('image', default='placeholder')
+    content = models.TextField(max_length=2000, null=False, blank=False)
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    excerpt = models.TextField(blank=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    meal_type = models.CharField(max_length=50, choices=MEAL_TYPES, default="fish")
+    effort = models.CharField(
+        max_length=50, choices=EFFORT, default="Bad day comfort food"
+    )
+    """
+    Need to install django-resized==version?
+    v2image = ResizedImageField(
+        size=[400, None],
+        quality=75,
+        upload_to="recipes/",
+        force_format="WEBP",
+        blank=False,
+        null=False,
+    )
+    """
+    image_alt = models.CharField(max_length=100, null=False, blank=False)
+
+    class Meta:
+        ordering = ["-created_on"]
+
+    def __str__(self):
+        return f'{self.title} | added by {self.user}'
--- foolproflavors/settings.py
+++ foolproflavors/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*4q6p)_v!%9e96%(yki8m@9g+tpvkg)o+@du3)$-&d7e$=^c)_'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -90,6 +90,10 @@ DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.gitpod.io",
+    "https://*.herokuapp.com"
+]
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
