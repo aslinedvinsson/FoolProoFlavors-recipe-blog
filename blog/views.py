a8117364eed9673806436f470daa4e0430db7f94
Ann Edvinsson <aslin.edvinsson@gmail.com>
2024-01-27 15:39:24 +0000

    Add recipepost_detail.html and view

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import RecipePost
 
@@ -8,3 +8,25 @@ class RecipePostList(generic.ListView):
     template_name = "blog/index.html"
     paginate_by = 6
 
+def recipepost_detail(request, slug):
+    """
+    Display an individual :model:`blog.Recipepost`.
+
+    **Context**
+
+    ``recipepost``
+        An instance of :model:`blog.RecipePost`.
+
+    **Template:**
+
+    :template:`blog/recipepost_detail.html`
+    """
+
+    queryset = RecipePost.objects.filter(status=1)
+    recipepost = get_object_or_404(queryset, slug=slug)
+
+    return render(
+        request,
+        "blog/recipepost_detail.html",
+        {"recipepost": recipepost},
+    )
