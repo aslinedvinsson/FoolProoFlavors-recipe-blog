4306962a3ac005ccd58559fa05bb4d7dcb4371aa
Ann Edvinsson <aslin.edvinsson@gmail.com>
2024-01-28 12:16:52 +0000

    Add title to About model and add image

--- about/models.py
+++ about/models.py
@@ -6,6 +6,7 @@ class About(models.Model):
     A model to create and manage content in an About page
     ********related to :model:`auth.User`
     """
+    title = models.CharField(max_length=200)
     content = models.TextField()
     updated_on = models.DateTimeField(auto_now=True)
 
