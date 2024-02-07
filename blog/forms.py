e7b10c6d9a0ffde3fefa39f436400665d05a5f92
Ann Edvinsson <aslin.edvinsson@gmail.com>
2024-02-07 07:55:49 +0000

    Make the resize image in the form.py instead of the model

--- blog/forms.py
+++ blog/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from .models import Comment
 from .models import RecipeRating
 from .models import RecipePost
+from PIL import Image
 
 
 
@@ -27,12 +28,24 @@ class RatingForm(forms.ModelForm):
 class RecipePostForm(forms.ModelForm):
     existing_image_public_id = forms.CharField(widget=forms.HiddenInput, required=False)
 
-
-
     class Meta:
         model = RecipePost
         fields = ['title', 'excerpt', 'ingredients', 'instructions', 'meal_type', 'effort', 'food_image', 'image_alt']
 
-    def __init__(self, *args, **kwargs):
-        super(RecipePostForm, self).__init__(*args, **kwargs)
+    def save(self, commit=True):
+        instance = super().save(commit=False)
+        existing_image_public_id = self.cleaned_data.get('existing_image_public_id')
+        if existing_image_public_id:
+            instance.food_image = existing_image_public_id
+        if 'food_image' in self.changed_data:  # Check if food_image is being updated
+            image = instance.food_image
+            with Image.open(image.path) as img:
+                # Resize the image
+                img.thumbnail((500, 500))
+                img.save(image.path)
+        if commit:
+            instance.save()
+        return instance
 
+    #def __init__(self, *args, **kwargs):
+     #   super(RecipePostForm, self).__init__(*args, **kwargs)
