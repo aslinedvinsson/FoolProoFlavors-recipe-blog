7ef56a355753492c2d01db0b7e60ff04a0ba014e
Ann Edvinsson <aslin.edvinsson@gmail.com>
2024-02-23 05:14:01 +0000

    Cleaning up recipe.js

--- static/js/recipe.js
+++ static/js/recipe.js
@@ -1,74 +1,20 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const deleteRecipeModalEl = document.querySelector("#deleteRecipeModal");
-    const deleteRecipeModal = bootstrap.Modal.getOrCreateInstance(deleteRecipeModalEl);
-    const deleteRecipeConfirm = document.getElementById("deleteRecipeConfirm");
-    const deleteRecipeButtons = document.getElementsByClassName("delete_button_recipe");
-
-    startUpdateFunctionality();
-    if (deleteRecipeButtons.length > 0) {
-        console.log(bootstrap.Modal);
-        startDeleteFunctionality(deleteRecipeButtons, deleteRecipeModal, deleteRecipeConfirm);
-    }
-});
-
-function startUpdateFunctionality() {
-    const updateRecipeButtons = document.getElementsByClassName("update_button_recipe");
-    for (let button of updateRecipeButtons) {
-        button.addEventListener("click", (e) => {
-            let updateUrl = e.target.getAttribute("data-update_url");
-            window.location.href = updateUrl;
-        });
-    }
-}
-
-function startDeleteFunctionality(deleteRecipeButtons, deleteRecipeModal, deleteRecipeConfirm) {
-    for (let button of deleteRecipeButtons) {
-        button.addEventListener("click", (e) => {
-            e.preventDefault();
-            let recipeSlug = e.target.getAttribute("data-recipepost_slug");
-            deleteRecipeConfirm.href = `/delete_recipe/${recipeSlug}/`;
-            deleteRecipeModal.show();
-        });
-    }
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
 // Inspired by the CodeInstitute Walkthrough 'I think, therefore I blog'
 document.addEventListener("DOMContentLoaded", function() {
-    const updateRecipeButtons = document.getElementsByClassName("update_button_recipe");
-    const deleteRecipeButtons = document.getElementsByClassName("delete_button_recipe");
-    //const deleteRecipeModal = new bootstrap.Modal(document.getElementById("deleteRecipeModal"));
+    const updateRecipeButtons = document.getElementsByClassName(
+        "update_button_recipe");
+    const deleteRecipeButtons = document.getElementsByClassName(
+        "delete_button_recipe");
+    const deleteRecipeModal = new bootstrap.Modal(document.getElementById(
+        "deleteRecipeModal"));
     const deleteRecipeConfirm = document.getElementById("deleteRecipeConfirm");
 
-    // const deleteRecipeModalEl = document.getElementById("deleteRecipeModal");
-    const deleteRecipeModalEl = document.querySelector("#deleteRecipeModal");
-
-
-    //const deleteRecipeModalEl = document.querySelector("#deleteRecipeModal");
-    const deleteRecipeModal = bootstrap.Modal.getOrCreateInstance(deleteRecipeModalEl);
-
-
-
-
-    /**
+   /**
      * Initializes edit functionality for the provided edit buttons.
      *
      * For each button in the `editRecipeButtons` collection:
      * - Retrieves the associated recipe's ID upon click.
      * - Opens the edit modal (`editRecipeModal`) to allow editing.
+     */
 
 
     // Edit functionality
@@ -88,7 +34,7 @@ document.addEventListener("DOMContentLoaded", function() {
     * deletion endpoint for the specific recipe.
     * - Displays a confirmation modal (`deleteRecipeModal`) to prompt
     * the user for confirmation before deletion.
-
+    */
 
     // Deletion functionality
     for (let button of deleteRecipeButtons) {
@@ -100,6 +46,3 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     }
 });
-
-
-*/
