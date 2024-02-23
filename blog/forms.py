debb946f71621a4390ae24feb7775a2e46f268fb
Ann Edvinsson <aslin.edvinsson@gmail.com>
2024-02-23 05:12:50 +0000

    Add excerpt tp SummernotWidget in forms.py

--- blog/forms.py
+++ blog/forms.py
@@ -88,4 +88,5 @@ class RecipePostForm(forms.ModelForm):
         widgets = {
             'ingredients': SummernoteWidget(),
             'instructions': SummernoteWidget(),
+            'excerpt': SummernoteWidget(),
         }
